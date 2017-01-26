9d784f6939e26e3eeabc6d33b1181bafd6645496
Vlad Zakharo <vlad.zakharo@gmail.com>
2017-01-26 14:21:45 +0300

    test

--- app/src/main/java/com/example/vladzakharo/androidproject/Constants/Constants.java
+++ app/src/main/java/com/example/vladzakharo/androidproject/Constants/Constants.java
@@ -7,6 +7,4 @@ package com.example.vladzakharo.androidproject.constants;
 public class Constants {
     public static final String URL = "http://www.mocky.io/v2/58725bb10f00009512c6ac99";
     public static final String DISK_CACHE_SUBDIR = "thumbnails";
-    public static final String SMALL_PHOTO = "small_photo";
-    public static final String FULL_PHOTO = "full_photo";
 }
